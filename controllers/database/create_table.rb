719d941ca89aafd8de84b34c9038c9410612b33f
udon <matcha@dan5.me>
2025-01-03 14:33:25 +0900

    app.rbにdb&tableを作成するプログラムを追加:\Projects\bbs_sample\README.md

--- controllers/database/create_table.rb
+++ controllers/database/create_table.rb
@@ -1,31 +1,38 @@
 require 'sqlite3'
 
-# SQLite3データベースに接続
-db = SQLite3::Database.new "bbs.db"
+def create_table()
+    # SQLite3データベースに接続
+    db = SQLite3::Database.new "bbs.db"
 
-# threadsテーブルの作成
-db.execute <<-SQL
-  CREATE TABLE IF NOT EXISTS threads (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name TEXT NOT NULL,
-    title TEXT NOT NULL,
-    content TEXT NOT NULL,
-    datetime DATETIME DEFAULT CURRENT_TIMESTAMP
-  );
-SQL
+    # threadsテーブルの作成
+    db.execute <<-SQL
+    CREATE TABLE IF NOT EXISTS threads (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT NOT NULL,
+        title TEXT NOT NULL,
+        content TEXT NOT NULL,
+        datetime DATETIME DEFAULT CURRENT_TIMESTAMP
+    );
+    SQL
 
-# replysテーブルの作成
-db.execute <<-SQL
-  CREATE TABLE IF NOT EXISTS replys (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    thread_id INTEGER NOT NULL,
-    reply_number INTEGER NOT NULL,
-    name TEXT NOT NULL,
-    content TEXT NOT NULL,
-    datetime DATETIME DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (thread_id) REFERENCES threads(id)
-  );
-SQL
+    # replysテーブルの作成
+    db.execute <<-SQL
+    CREATE TABLE IF NOT EXISTS replys (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        thread_id INTEGER NOT NULL,
+        reply_number INTEGER NOT NULL,
+        name TEXT NOT NULL,
+        content TEXT NOT NULL,
+        datetime DATETIME DEFAULT CURRENT_TIMESTAMP,
+        FOREIGN KEY (thread_id) REFERENCES threads(id)
+    );
+    SQL
 
-# DBの閉じる
-db.close
+    true
+rescue => e
+    puts "Error create table : #{e.message}"
+    false
+ensure
+    # DBを閉じる
+    db.close
+end
